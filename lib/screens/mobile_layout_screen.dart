341f2048d4261d5b4b022d415cb670b14e997485
Abdul Rahim <113752743+nextive-rahim@users.noreply.github.com>
2023-06-08 12:51:21 +0600

    fix : update project in flutter latest version.(2.12.0 to 3.0.3)

--- lib/screens/mobile_layout_screen.dart
+++ lib/screens/mobile_layout_screen.dart
@@ -1,13 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:whatsapp_ui/colors.dart';
 
 class MobileLayoutScreen extends StatelessWidget {
   const MobileLayoutScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Text('Mobile!'),
+    return const Scaffold(
+      body: Center(child: Text('Mobile!')),
     );
   }
 }
